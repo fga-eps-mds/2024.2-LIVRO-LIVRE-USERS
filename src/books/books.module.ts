64f67e58839676759b0e00c9a3d90152f42a9643
Bruno Cruz <Bruno.g.rosa@hotmail.com.br>
2025-01-18 12:36:40 -0300

    feat: change on modules

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([])],
   controllers: [BooksController],
   providers: [BooksService],
 })
